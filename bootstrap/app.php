59a986130820291babf8939827cc9a7fbf969c73
Emanuele2295 <emanuelemorgillo2295@gmail.com>
2026-04-27 00:27:31 +0200

    Fix e test per bloccare commenti malevoli ripetitivi con BlockSuspiciousIPs in Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'block_suspicius_ips' => \App\Http\Middleware\BlockSuspiciousIPs::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
